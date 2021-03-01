6c9dcb7cac7cb49eeb1bef390e9358a5bf222af4
eauguet011h <enric.auguet@011h.com>
2021-03-01 16:11:30 +0100

    up

--- models/stagying/stg_011h_classification.sql
+++ models/stagying/stg_011h_classification.sql
@@ -1,6 +1,8 @@
 with source as (
-
-    SELECT * FROM `mvp-011h.RAW_Constructive_System_TEMP.011h_classification`
+        
+        
+    select * from {{ source('Bigquery','011h_classifications') }}
+    
     
 )
 select * from source
